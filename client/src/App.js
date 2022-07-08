4c6ab75f9bbcd081fea9327353e584bc713063d3
tylerdurden158 <aryamanjaiswal23@gmail.com>
2022-07-08 19:39:07 +0530

    edit

--- client/src/App.js
+++ client/src/App.js
@@ -1,39 +1,3 @@
-// import React,{useEffect,useState} from 'react'
-// import {Container,AppBar,Typography,Grid,Grow} from'@material-ui/core'
-// import { useDispatch } from 'react-redux'
-// import {getPosts} from './actions/posts'
-// import memories from './images/memories.png'
-// import Posts from './components/Posts/Posts'
-// import Form from './components/Form/Form'
-// import useStyles from './styles.js'
-// const App = () => {
-//     const [currentId,setCurrentId]=useState(null)
-//     const classes=useStyles()
-//     const dispatch = useDispatch()
-//     useEffect(()=>{
-//         dispatch(getPosts())
-//     },[currentId,dispatch])
-//     return (
-//         <Container maxWidth="lg">
-//             <AppBar className={classes.AppBar} position='static' color='inherit'>
-//             <Typography className={classes.heading} variant='h2' align='center'>Memories</Typography>
-//             <img className={classes.image} src={memories} alt='memories' height="60" />
-//             </AppBar>
-//             <Grow in>
-//                 <Grid container justify='sapce-between' alignItems='stretch' spacing={3}>
-//                     <Grid item xs={12} sm={7}>
-//                         <Posts setCurrentId={setCurrentId} />
-//                     </Grid>
-//                     <Grid item xs={12} sm={7}>
-//                         <Form currentID={currentId} setCurrentId={setCurrentId} />
-//                     </Grid>
-//                 </Grid>
-//             </Grow>
-//         </Container>
-//     )
-// }
-// export default App
-
 import React, { useState, useEffect } from 'react';
 import { Container, AppBar, Typography, Grow, Grid } from '@material-ui/core';
 import { useDispatch } from 'react-redux';
